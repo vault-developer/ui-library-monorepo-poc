c05de23ef9e91d766801b6c18bd2693a82f80770
albert <trott.alik@gmail.com>
2021-12-05 23:52:47 +0500

    chore: add ui-form build config

---
+++ packages/ui-form/src/ui-form.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Button} from '@vault-developer/ui-button';
+
+const Form = () => (
+    <form>
+        <Button style={{background: 'green'}}>HELLO</Button>
+    </form>
+);
+export {Form};
